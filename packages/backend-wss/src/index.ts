b85e4b4c6f93d8898da510257b5966c3af5dc9ec
sheninthjr <sheninthjr23@gmail.com>
2023-12-22 17:12:34 +0530

    Basic Websocket Initialized

--- packages/backend-wss/src/index.ts
+++ packages/backend-wss/src/index.ts
@@ -9,6 +9,41 @@ const server = http.createServer(app);
 
 const wss = new WebSocketServer({ server })
 
-app.listen(PORT, () => {
-    console.log(`Server started on http://localhost:${PORT}`);
+const users: {
+  [key: string]: {
+    room: string,
+    ws: any
+  }
+} = {}
+
+let counter = 0;
+wss.on("connection", async (ws) => {
+  const wsId = counter++;
+  ws.on("message", (message: string) => {
+    const data = JSON.parse(message.toString());
+    if (data.type === "join") {
+      users[wsId] = {
+        room: data.payload.roomId,
+        ws
+      }
+    }
+    if (data.type === "message") {
+      const message = data.payload.message;
+      const roomId = users[wsId].room;
+      Object.keys(users).forEach((wsId) => {
+        if (users[wsId].room === roomId) {
+          users[wsId].ws.send(JSON.stringify({
+            type: "message",
+            payload: {
+              message
+            }
+          }))
+        }
+      })
+    }
+  })
+})
+
+server.listen(PORT, () => {
+  console.log(`Server started on http://localhost:${PORT}`);
 });
