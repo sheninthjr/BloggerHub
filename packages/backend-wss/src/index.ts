24585f92807efed23dc430c1566d6566a20ce365
sheninthjr <sheninthjr23@gmail.com>
2023-12-31 11:30:31 +0530

    Basic Schema Without propagation to frontend

--- packages/backend-wss/src/index.ts
+++ packages/backend-wss/src/index.ts
@@ -40,23 +40,47 @@ wss.on("connection", async (ws) => {
         receiverId: data.payload.receiverId,
         ws
       }
-    }
-    await prisma.chatUser.create({
-      data:{
-        id:"df",
-        room:"sf",
-        senderId:"sdfa",
-        receiverId:"afsd",
-        messages:"das"
+      const existing = await prisma.chatUser.findFirst({
+        where: {
+          AND: [
+            { senderId: data.payload.senderId },
+            { receiverId: data.payload.receiverId },
+          ],
+        }
+      })
+      if (!existing) {
+        await prisma.chatUser.create({
+          data: {
+            id: Math.floor(Math.random()*100000)+1,
+            senderId: data.payload.senderId,
+            receiverId: data.payload.receiverId
+          }
+        })
       }
-    })
+    }
+
     if (data.type === "message") {
       const message = data.payload.message;
       const senderId = data.payload.senderId;
       const receiverId = data.payload.receiverId;
       const roomId = users[wsId].room;
       userMessage[senderId] = message;
-
+      const existing = await prisma.chatUser.findFirst({
+        where: {
+          AND: [
+            { senderId: data.payload.senderId },
+            { receiverId: data.payload.receiverId },
+          ],
+        }
+      })
+      if (existing) {
+        await prisma.message.create({
+          data:{
+            authorId: existing.id,
+            message:message
+          }
+        })
+      }
       Object.keys(users).forEach((wsId) => {
         if (users[wsId].room === roomId) {
           users[wsId].ws.send(JSON.stringify({
